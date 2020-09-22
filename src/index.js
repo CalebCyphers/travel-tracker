339ea1f9ca2194ea327477aa042ba30027805312
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2020-09-22 15:56:16 -0600

    import TravelRepository into index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,18 @@
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
 import User from './User';
+import TravelRepository from './TravelRepository';
 import './images/turing-logo.png'
 
 let traveler = new User(10);
-traveler.getUserData()
-setTimeout(function() { console.log(traveler); } , 3000);
+let repository = new TravelRepository;
+
+traveler.getUserData();
+repository.getDestinations();
+
+setTimeout(function() { 
+  console.log(traveler)
+  console.log(repository)
+}, 3000);
 
 console.log('This is the JavaScript entry file - your code begins here.');
