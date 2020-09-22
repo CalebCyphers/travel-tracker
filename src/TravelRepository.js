f54aae2d84157a9113207bee3f943a9f94cc6ee8
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2020-09-22 15:55:09 -0600

    add getDestinations function to the TravelRepository.js

--- src/TravelRepository.js
+++ src/TravelRepository.js
@@ -1,6 +1,12 @@
 class TravelRepository {
   constructor() {
-    
+
+  }
+
+  getDestinations () {
+    fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/destinations/destinations')
+      .then(response => response.json())
+      .then(data => this.destinations = data.destinations)
   }
 }
 
