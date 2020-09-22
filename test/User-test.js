7c5121fd324696c698e532ca26dd184d815acd3e
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2020-09-22 16:55:36 -0600

    Add test data

--- test/User-test.js
+++ test/User-test.js
@@ -8,6 +8,33 @@ describe('User', () => {
   beforeEach(() => {
     let testRepository = new TravelRepository()
     let testUser = new User(1, testRepository)
+
+    testRepository.destinations = [
+      {
+        alt: "a picture of cool stuff",
+        destination: "Place, Bigger Place",
+        estimatedFlightCostPerPerson: 400,
+        estimatedLodgingCostPerDay: 70,
+        id: 1,
+        image: "https://images.unsplash.com/photo-1489171084"
+      },
+      {
+        alt: "a picture of lame stuff",
+        destination: "Thingsville, Thingland",
+        estimatedFlightCostPerPerson: 200,
+        estimatedLodgingCostPerDay: 500,
+        id: 2,
+        image: "https://images.unsplash.com/photo-1489171084"
+      },
+      {
+        alt: "a picture of weird stuff",
+        destination: "Stuff, Stuffland",
+        estimatedFlightCostPerPerson: 40,
+        estimatedLodgingCostPerDay: 40,
+        id: 3,
+        image: "https://images.unsplash.com/photo-1489171084"
+      }
+    ]
   })
 
 
