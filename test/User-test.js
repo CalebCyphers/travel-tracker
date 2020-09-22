fca2d62d79ec2f83243c087f0056de0ab90d6342
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2020-09-22 17:05:26 -0600

    add trip data for testing

--- test/User-test.js
+++ test/User-test.js
@@ -35,6 +35,49 @@ describe('User', () => {
         image: "https://images.unsplash.com/photo-1489171084"
       }
     ]
+
+    testRepository.trips = [
+      {
+        date: "2019/09/16",
+        destinationID: 2,
+        duration: 8,
+        id: 1,
+        status: "approved",
+        suggestedActivities: [],
+        travelers: 1,
+        userID: 2
+      },
+      {
+        date: "2020/10/04",
+        destinationID: 3,
+        duration: 3,
+        id: 2,
+        status: "pending",
+        suggestedActivities: [],
+        travelers: 5,
+        userID: 2
+      },
+      {
+        date: "2020/05/22",
+        destinationID: 1,
+        duration: 17,
+        id: 3,
+        status: "approved",
+        suggestedActivities: [],
+        travelers: 4,
+        userID: 2
+      },
+      {
+        date: "2020/02/25",
+        destinationID: 3,
+        duration: 10,
+        id: 4,
+        status: "pending",
+        suggestedActivities: [],
+        travelers: 2,
+        userID: 1
+      }
+    ]
   })
 
 
