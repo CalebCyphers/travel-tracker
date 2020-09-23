02d37a9c7426f67596377907303032911a9f8549
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2020-09-23 13:10:04 -0600

    Refactor User.js

--- src/User.js
+++ src/User.js
@@ -3,13 +3,19 @@ import time from '../src/Time.js';
 class User {
   constructor(userId, travelData) {
     this.userId = userId
-    this.upcomingTrips = travelData.trips.filter(trip => {
+    this.upcomingTrips = this.findUpcomingTrips(travelData)
+  }
+
+  findUpcomingTrips(travelData) {
+    return travelData.trips.filter(trip => {
       let today = time.buildDate(travelData.currentDay)
       let future = time.daysFromDate(today, 365)
       let departure = time.buildDate(trip.date)
       return trip.status === "approved" ? time.isBetween(today, departure, future) : false
     })
   }
+
+  
 
   getUserData() {
     fetch(`https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers/${this.userId}`)
