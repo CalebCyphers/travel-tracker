5336f0c9b6b876737546ebbe3776c731bee93a10
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2020-09-23 18:04:14 -0600

    Add domupdates.js and node.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,19 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+import node from './node.js';
+import domUpdate from 'domUpdates.js';
 import User from './User';
 import TravelRepository from './TravelRepository';
-import './images/turing-logo.png'
 
 let traveler;
 let repository = new TravelRepository;
 repository.getDestinations();
 repository.getTrips();
-
 setTimeout(() => {
-  traveler = new User(getRandomUserID(), repository);
+  traveler = new User(Math.floor(Math.random() * 20), repository);
   traveler.getUserData();
-}, 1000)
-
-let getRandomUserID = () => {
-  return Math.floor(Math.random() * 20)
-}
+}, 100)
 
 setTimeout(function() { 
   console.log(traveler)
   console.log(repository)
-}, 3000);
-
-console.log('This is the JavaScript entry file - your code begins here.');
+}, 101);
